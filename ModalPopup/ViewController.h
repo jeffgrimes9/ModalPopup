3affe73864836e28d8d7093adb1e75043e8d9e6b
Jeff Grimes <jeffgrimes9@gmail.com>
2012-08-21 12:07:35 -0700

    created project and view

---
+++ ModalPopup/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ModalPopup
+//
+//  Created by Jeff Grimes on 8/21/12.
+//  Copyright (c) 2012 Jeff Grimes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
